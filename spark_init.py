dcf9cc2ad3b4f4a23a41df1b3203b026e71079c3
noricks <476852755@qq.com>
2022-05-01 17:24:18 +0800

    remove useless files

--- spark_init.py
+++
@@ -1,6 +0,0 @@
-# %%
-from pyspark import SparkConf, SparkContext
-conf = SparkConf().setMaster("local[*]").setAppName("My App")
-sc = SparkContext(conf = conf)
-a = sc.parallelize([1,2,3])
-a.count()
